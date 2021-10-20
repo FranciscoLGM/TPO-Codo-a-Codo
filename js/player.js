b44f35014775867b30a3504b43134c2d03b5f14e
Francisco Javier López García Mayorga <javier6282@gmail.com>
2021-10-20 20:51:24 -0300

    new changes

---
+++ js/player.js
@@ -0,0 +1,3 @@
+let myPlayerDiv = document.getElementById("player"); // Nos devuelve ese Div
+
+myPlayerDiv.innerHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/dp5J1FZnGVg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
