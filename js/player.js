2b269a3a23e7fc7b669b48635d3749bc9e9faac7
Francisco Javier López García Mayorga <javier6282@gmail.com>
2021-10-25 20:09:51 -0300

    Index Responsive

--- js/player.js
+++ js/player.js
@@ -1,3 +1,16 @@
-let myPlayerDiv = document.getElementById("player"); // Nos devuelve ese Div
+/*function player(extension) {
+    let trailer = "https://www.youtube.com/embed/"
+    let video = trailer + extension
+    let myPlayer = document.getElementById("player");
+    myPlayer.innerHTML = '<iframe width="100%" height="100%" src= ' + video + ' title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+}
 
-myPlayerDiv.innerHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/dp5J1FZnGVg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+player("dp5J1FZnGVg")*/
+
+const movies = document.getElementById('movies');
+
+movies.addEventListener('click', (e) => {
+    if (e.target.id !== 'movies'){
+        alert('click');
+    } 
+});
